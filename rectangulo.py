3285eb34fbc39c6b5b5a3441ee01ddf5d127be53
JorgeFierroo <135017288+JorgeFierroo@users.noreply.github.com>
2023-09-04 11:31:15 -0300

    Update rectangulo.py

--- rectangulo.py
+++ rectangulo.py
@@ -1,15 +1,24 @@
-
-class Cectangulo:
-    def __init__(self,longitud,ancho):
-        self.longitud = longitud
-        self.ancho = ancho
+class Circulo:
+    def __init__(self,radio):
+        self.radio = radio
         
-    #metodos
-    def CalcularArea(longitud,ancho):
-        area = longitud*ancho
-        return f"El area del rectangulo es: {area}"
+    def CalcularArea(radio):
+        area = (radio**2)*3.14
+        return f"El area del circulo es: {area}"
     
-    def CalcularPerimetro(longitud,ancho):
-        perimetro = (longitud*2)+(ancho*2)
-        return f"El perimetro del rectangulo es: {perimetro}"
+    def CalcularPerimetro(radio):
+        perimetro = (2*radio)*3.14
+        return f"El perimetro del circulo es: {perimetro}"
     
+    def cambiar_radio(self, cambiar):
+        self.radio = cambiar
+        return f"el nuevo radio es: {self.radio}"
+    
+circulo0 = Circulo(4)
+circulo1 = Circulo.CalcularArea(circulo0.radio)
+circulo2 = Circulo.CalcularPerimetro(circulo0.radio)
+print(circulo1)
+print(circulo2)
+circulo3 = circulo0.cambiar_radio(7)
+print(circulo3)
+
